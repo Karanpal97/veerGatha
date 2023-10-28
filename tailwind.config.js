70ef831e5ba3fd62fa9e2d7be7cb86ee3a21c96e
Shryansh <shryansh2024@gmail.com>
2023-10-28 22:01:19 +0530

    fix deployment error try  4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
+/** @type {import('tailwindcss').Config} */
+
 module.exports = withMT({
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
-        extend: {
-            fontFamily: {
-                poppins: ["Poppins", "sans"],
-            },
-        },
+        extend: {},
     },
     plugins: [],
 });
